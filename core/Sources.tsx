164d0918af4f5abc4b8782ec4092898fd3b82bdc
Kaan Mol <kaanmol@outlook.com>
2022-12-22 10:06:28 +0100

    Added react-hook-forms

--- core/Sources.tsx
+++ core/Sources.tsx
@@ -1,16 +1,67 @@
-import {Text, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import {useRecoilValue} from 'recoil';
 import {sourceSelector} from './store/source';
+import {useForm, SubmitHandler, Controller, Control} from 'react-hook-form';
 
-export const Sources = () => {
+type SourceInput = {
+  name: string;
+  url: string;
+};
+
+const Input = ({
+  name,
+  control,
+}: {
+  name: any;
+  control: Control<SourceInput, any>;
+}) => {
+  return (
+    <Controller
+      control={control}
+      rules={{
+        required: true,
+      }}
+      render={({field: {onChange, onBlur, value}}) => (
+        <TextInput
+          onBlur={onBlur}
+          onChangeText={onChange}
+          placeholder={name}
+          value={value}
+        />
+      )}
+      name={name}
+    />
+  );
+};
+
+export function Sources() {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<SourceInput>({
+    defaultValues: {
+      name: '',
+      url: '',
+    },
+  });
+  const onSubmit: SubmitHandler<SourceInput> = data => console.log(data);
+
   const sourceList = useRecoilValue(sourceSelector);
   return (
     <View>
-      {sourceList.map((source: {url: String; name: String}) => (
+      {sourceList.map((source: {url: string; name: string}) => (
         <Text>
           sourceName: {source.name}, sourceURL: {source.url}
         </Text>
       ))}
+
+      <View style={{marginTop: 60}}>
+        <Text style={{fontSize: 24, marginBottom: 15}}>Add event source</Text>
+        <Input name="name" control={control} />
+        <Input name="url" control={control} />
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
     </View>
   );
-};
+}
