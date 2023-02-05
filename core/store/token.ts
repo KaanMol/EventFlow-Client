ee64de20e4fa4f3f6c99c09d5e3321a1a83829dd
Kaan <kaanmol@outlook.com>
2023-02-05 02:06:29 +0100

    Changed import of storage

--- core/store/token.ts
+++ core/store/token.ts
@@ -1,26 +1,26 @@
 import axios from "axios";
-import EncryptedStorage from "react-native-encrypted-storage";
 import { atom, selector } from "recoil";
 import { request } from "../common/request";
+import { Storage } from "../common/storage.web";
 
 const tokenAtom = atom({
-    key: 'token',
-    default: selector({
-        key: 'sourceLoader',
-        get: async () => {
-            return await EncryptedStorage.getItem('access_token');
-        }
-    }),
+	key: 'token',
+	default: selector({
+		key: 'sourceLoader',
+		get: async () => {
+			return await Storage.getItem('access_token');
+		}
+	}),
 });
 
 export const tokenState = selector({
-    key: 'tokenState',
-    get: ({ get }) => {
-        const token = get(tokenAtom);
-        if (token === null) { return null; } else { return token; }
-    },
-    set: ({ set }, newValue) => {
-        EncryptedStorage.setItem('access_token', newValue === null ? '' : newValue.toString());
-        return set(tokenAtom, newValue);
-    }
+	key: 'tokenState',
+	get: ({ get }) => {
+		const token = get(tokenAtom);
+		if (token === null) { return null; } else { return token; }
+	},
+	set: ({ set }, newValue) => {
+		Storage.setItem('access_token', newValue === null ? '' : newValue.toString());
+		return set(tokenAtom, newValue);
+	}
 });
