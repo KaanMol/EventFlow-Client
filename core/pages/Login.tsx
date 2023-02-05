6dbf7ab2523bf3a32156eeb1b91d551ca26ca24e
Kaan <kaanmol@outlook.com>
2023-02-05 02:06:11 +0100

    Moved inappbrowser

--- core/pages/Login.tsx
+++ core/pages/Login.tsx
@@ -1,12 +1,13 @@
-import EncryptedStorage from 'react-native-encrypted-storage';
+
 import axios from 'axios';
 import Hashes from 'jshashes';
 import randomString from 'random-string';
 import { ActivityIndicator, Alert, Button, Linking, Text, View } from 'react-native';
-import InAppBrowser from 'react-native-inappbrowser-reborn';
+
 import authConfig from '../config/auth';
 import { FullPageLayout } from '../layout/page/FullPage';
 import { useState } from 'react';
+import { Storage } from "../common/storage.web";
 
 function LoggingIn() {
 	return <>
@@ -37,61 +38,29 @@ export function Login(props: { setLogin: (token: string) => void }) {
 					' ',
 				)}&state=${authState}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
 
-			if (await InAppBrowser.isAvailable()) {
-				const result = await InAppBrowser.openAuth(url, 'my-demo://demo/', {
-					// iOS Properties
-					dismissButtonStyle: 'cancel',
-					preferredBarTintColor: '#453AA4',
-					preferredControlTintColor: 'white',
-					readerMode: false,
-					animated: true,
-					modalPresentationStyle: 'fullScreen',
-					modalTransitionStyle: 'coverVertical',
-					modalEnabled: true,
-					enableBarCollapsing: false,
-					ephemeralWebSession: false,
-					// Android Properties
-					showTitle: true,
-					toolbarColor: '#6200EE',
-					secondaryToolbarColor: 'black',
-					navigationBarColor: 'black',
-					navigationBarDividerColor: 'white',
-					enableUrlBarHiding: true,
-					enableDefaultShare: true,
-					forceCloseOnRedirection: false,
-					// Specify full animation resource identifier(package:anim/name)
-					// or only resource name(in case of animation bundled with app).
-					animations: {
-						startEnter: 'slide_in_right',
-						startExit: 'slide_out_left',
-						endEnter: 'slide_in_left',
-						endExit: 'slide_out_right',
-					},
-				});
 
-				setLoggingIn(true);
+			const result = setLoggingIn(true);
 
-				const code = (result as any).url.substring(21, 53);
-				const params = new URLSearchParams();
-				params.append('grant_type', 'authorization_code');
-				params.append('client_id', authConfig.clientId);
-				params.append('code_verifier', codeVerifier);
-				params.append('code', code);
-				params.append('redirect_uri', 'my-demo://demo/');
+			const code = (result as any).url.substring(21, 53);
+			const params = new URLSearchParams();
+			params.append('grant_type', 'authorization_code');
+			params.append('client_id', authConfig.clientId);
+			params.append('code_verifier', codeVerifier);
+			params.append('code', code);
+			params.append('redirect_uri', 'my-demo://demo/');
 
-				const res = await axios.post(
-					authConfig.tokenEndpoint,
-					params.toString(),
-				);
-				await EncryptedStorage.setItem('access_token', res.data.access_token);
-				props.setLogin(res.data.access_token);
-				await EncryptedStorage.setItem('refresh_token', res.data.refresh_token);
-			} else {
-				Linking.openURL(url);
-			}
+			const res = await axios.post(
+				authConfig.tokenEndpoint,
+				params.toString(),
+			);
+
+			await Storage.setItem('access_token', res.data.access_token);
+			props.setLogin(res.data.access_token);
+			await Storage.setItem('refresh_token', res.data.refresh_token);
+
 		} catch (error) {
 			console.log(JSON.stringify(error));
-			Alert.alert(error);
+			Alert.alert(error as any);
 		}
 	};
 
