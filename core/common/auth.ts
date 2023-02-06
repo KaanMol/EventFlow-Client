21edb666d5b58132aed83c004253465d9d3aec9f
Kaan <kaanmol@outlook.com>
2023-02-06 20:06:44 +0100

    Cleaned up auth flow starters methods

--- core/common/auth.ts
+++ core/common/auth.ts
@@ -1,18 +1,22 @@
 import { Linking } from 'react-native';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
+import { NavigateFunction } from 'react-router-dom';
 import auth from '../config/auth';
-import { fetchAccessToken, generateAuthPrerequisites } from './token';
+import { generateAuthPrerequisites } from './token';
 
-export async function startAuthFlow() {
+export async function startAuthFlow(navigate: NavigateFunction) {
 	const { url } = await generateAuthPrerequisites();
 	const result = await startInAppBrowser(url);
-	return await fetchAccessToken((result as any).url.substring(21, 53))
+	const code = (result as any).url.substring(21, 53);
+
+	navigate({
+		pathname: '/',
+		search: `?code=${code}`
+	});
 };
 
 async function startInAppBrowser(url: string) {
-	console.log(await InAppBrowser.isAvailable())
 	if (await InAppBrowser.isAvailable() === false) {
-
 		return Linking.openURL(url);
 	}
 
--- core/common/auth.web.ts
+++ core/common/auth.web.ts
@@ -1,14 +1,10 @@
-import { Linking } from "react-native";
 import { Storage } from "./storage";
 import { generateAuthPrerequisites } from "./token";
-
-export async function startAuth(url: string) {
-	// TODO: IMPLEMENT AUTH
-	return "IMPLEMENT ME"
-}
 
 export async function startAuthFlow() {
 	const { codeVerifier, url } = await generateAuthPrerequisites();
 	await Storage.setItem('auth.code_verifier', codeVerifier);
-	Linking.openURL(url);
+
+	// FIX ME: Find an alternative of redirecting the user for the auth flow
+	(window as any).location.href = url;
 };
