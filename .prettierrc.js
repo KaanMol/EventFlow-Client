cdd6c149f38a8ea65ede8c98ad0d88e41bf74fd5
Lucas <somfic@gmail.com>
2023-03-13 22:58:34 +0100

    Updated Prettier config for more consistent code between collaborators

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+	arrowParens: "avoid",
+	bracketSameLine: true,
+	bracketSpacing: false,
+	singleQuote: true,
+	trailingComma: "all",
+	useTabs: true,
+	bracketSpacing: true,
+	singleQuote: false,
 };
